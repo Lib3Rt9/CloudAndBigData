755654ac1520aa2b9cd8ed92c122b779d276458b
Lib3Rt9 <phamgiaphuc011709@gmail.com>
2024-10-20 22:21:10 +0700

    - Update more detailed instructions

--- begin.sh
+++ begin.sh
@@ -1,5 +1,17 @@
-echo "____________ Word Count example ____________"
+echo "_______________________ Word Count example _______________________"
 echo "with 1 master and 2 slaves"
 echo ""
+
+echo "Be flexible! XD"
+echo ""
+
+echo "To try another configuration, please either modify 'begin.sh' or directly run the command with this format:"
+echo ""
+echo "    ' bash start.sh [# of slave(s)] path/to/file.jar path/to/file_data jar_class_name '     "
+echo ""
+echo " XD __ (assuming there exists already a file.jar, knowing it's class name) __ XD"
+echo ""
+
 sleep 10
 bash start.sh 2 ./examples/wc.jar ./examples/filesample.txt WordCount
+
